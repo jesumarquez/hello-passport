0fe08d5b2f2ac1378c5a87c805e655ad6cde4b63
Jesú Márquez <jesumarquez@gmail.com>
2016-07-15 15:45:11 -0300

    missing dependencies. body-parser added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', express.static('public'));
 
